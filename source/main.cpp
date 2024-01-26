5b34ad58fe0064526152d1d356d084fb8b40ba61
schmid58peter <126481903+schmid58peter@users.noreply.github.com>
2024-01-26 11:34:48 +0100

    Update main.cpp
    Noch eine Ausgabe

--- source/main.cpp
+++ source/main.cpp
@@ -5,4 +5,5 @@ using namespace std;
 int main(int argc, char** argv)
 {
   std::cout << "This is my first branch on a project on GitHub" << std::endl;
+  std::cout << "A second line..." << std::endl;
 }
